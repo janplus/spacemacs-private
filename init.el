e202564d3da1762f65e4fb428e9158f285698a86
wujian <jan.chou.wu@gmail.com>
2017-08-12 16:32:19 +0800

    Enable CNFonts.

--- init.el
+++ init.el
@@ -317,6 +317,7 @@ explicitly specified that a variable should be set before a package is loaded,
 you should place your code here."
   ;; Fonts
   (use-package chinese-fonts-setup)
+  (cnfonts-enable)
 
   ;; Org-mode
   (setq spaceline-org-clock-p t)
