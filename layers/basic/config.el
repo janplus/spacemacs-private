b039260749d0eedb5d72482d4c10cb9386a7542e
wujian <jan.chou.wu@gmail.com>
2017-08-13 18:37:45 +0800

    Use Monday as the first day of Calendar.

---
+++ layers/basic/config.el
@@ -0,0 +1,18 @@
+;;; config.el --- Org configuration File for Spacemacs
+;;
+;; Copyright (c) 2017 Wu Jian
+;;
+;; Author: Wu Jian <jan.chou.wu@gmail.com>
+;; URL: https://github.com/janplus/spacemacs-private
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;;; Variables
+
+(setq user-full-name "Wu Jian")
+(setq user-mail-address "jan.chou.wu@gmail.com")
+
+;; Use Monday as the first day of Calendar
+(setq calendar-week-start-day 1)
